cf8a50689bda75a976048daf85a1b65e9bba3116
ArySalomon <arysalomon01@gmail.com>
2024-06-21 16:53:44 -0300

    ckan trials 21/6/2024

--- Ckan DTD/ckan_api.R
+++ Ckan DTD/ckan_api.R
@@ -1,14 +1,23 @@
-install.packages("ckanr")
 
+install.packages("ckanr")
 
 library(ckanr)
 library(httr)
 
-
+ckanr_setup(url = "https://datos.ciudaddemendoza.gob.ar", key = "")
 
 package_list()
 organization_list()
 group_list()
+
+# Buscar algún recurso de un paquete al que tengamos acceso como DTD a partir de las organizaciones
+organization_show(id = "transformacion_digital", include_datasets = TRUE, as = "list")$packages[[6]]
+
+# id = "c5d4c418-fc1b-4cad-b66d-acae566d5c6a"
+# title = "Bancos"
+# name = "bancos-ciudad"
+# url = "http://datos.ciudaddemendoza.gob.ar/dataset/bancos-ciudad"
+
 
 example_df <- data.frame(
   ID = 1:5,
@@ -17,58 +26,17 @@ example_df <- data.frame(
   City = c("New York", "Los Angeles", "Chicago", "Houston", "Phoenix")
 )
 
-
-
-# package_create(
-#   name = "my-new-dataset",
-#   title = "My New Dataset",
-#   private = F,
-#   author = "Dirección de Transformación Digital, Smart Cities y Gobierno Abierto",
-#   author_email = NULL,
-#   maintainer = NULL,
-#   maintainer_email = NULL,
-#   license_id = NULL,
-#   notes = NULL,
-#   package_url = NULL,
-#   version = NULL,
-#   state = "active",
-#   type = NULL,
-#   resources = NULL,
-#   tags = NULL,
-#   extras = NULL,
-#   relationships_as_object = NULL,
-#   relationships_as_subject = NULL,
-#   groups = list("Salud"),
-#   owner_org = "transformacion_digital",
-#   url = get_default_url(),
-#   key = get_default_key(),
-#   as = "list")
-
-
-
 write.csv(example_df, file = paste0(getwd(), "/example_df.csv"), row.names = FALSE)
 
 
-resource_create(
-  package_id = "acequias",
-  http_method = "POST",
-  rcurl = paste0(get_default_url(), "/dataset/", "acequias"),
-  url = get_default_url(),
-  key = get_default_key(),
+resource <- resource_create(
+  package_id = "bancos-ciudad",
   name = "Example Data Resource",
-  description = "exampledf",
+  description = "exampledf description",
   format = "CSV",
-  upload = paste0(getwd(), "/example_df.csv"),
-  as = "list"
+  mimetype = "text/csv",
+  upload = paste0(getwd(), "/example_df.csv")
 )
-
-
-
-
-
-
-
-
 
 
 
