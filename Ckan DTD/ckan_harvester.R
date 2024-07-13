f2e967dd7693c3126cc34be2d5300e37147a7dd1
ArySalomon <arysalomon01@gmail.com>
2024-07-13 15:01:30 -0300

    fakecommit

--- Ckan DTD/ckan_harvester.R
+++ Ckan DTD/ckan_harvester.R
@@ -4,6 +4,7 @@ library(httr)
 library(urltools)
 library(dplyr)
 library(tidyr)
+library(lubridate)
 
 ckanr_setup(url = "https://datos.ciudaddemendoza.gob.ar", key = "")
 
@@ -12,13 +13,12 @@ ping() # T = corriendo
 # Get the list of packages
 packages <- package_list(as = "table")
 
-# Initialize a list to store the data
-resources_data <- data.frame()
-
 # Loop through each package to get resource details
 
 rm(package_info, resources_df, resources_data, resource_n)
 
+# Initialize a list to store the data
+resources_data <- data.frame()
 
 # Loop through each package to get resource details
 for (package_id in packages) {
@@ -37,6 +37,7 @@ for (package_id in packages) {
       package_mantainer_mail = package_info$maintainer_email,
       last_updated = package_info$metadata_modified,
       act_periodicity = package_info$extras[which(package_info$extras$key == "accrualPeriodicity"), "value"],
+      timediff_acualizacion = as.numeric(difftime(today(), as.Date(as.POSIXct(package_info$metadata_modified), format = "%d-%m-%Y"))),
       
       resource_id = package_info$resource$id[resource_n],
       resource_name = package_info$resource$name[resource_n],
@@ -47,5 +48,9 @@ for (package_id in packages) {
   }
 }
 
+length(unique(resources_data$package_id))
+table(resources_data$act_periodicity)
 
 
+
+         
